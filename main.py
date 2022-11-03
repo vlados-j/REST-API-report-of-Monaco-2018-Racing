71193bd09308dd17ef9a23909639989e87ff96f8
Vladyslav Dzhulai <dzhulai.vladyslav@gmail.com>
2022-11-03 16:40:28 -0400

    Refactoring

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_restful import Api, Resource
-import json
-import xml.etree.ElementTree as ET
 from application_vlados import processing_data, build_report
+from dict2xml import dict2xml
 
 
 app = Flask(__name__)
@@ -15,10 +14,7 @@ class Report(Resource):
         structured_info = processing_data('files/start.log', 'files/end.log',
                                           'files/abbreviations.txt')
         prepared_info_for_report = info_for_output(structured_info, args.get("order"))
-        if args.get("format") == 'xml':
-            return generate_xml_data(prepared_info_for_report)
-        else:
-            return generate_json_data(prepared_info_for_report)
+        return generate_output_data(prepared_info_for_report, args.get("format"))
 
 
 class Drivers(Resource):
@@ -29,28 +25,19 @@ class Drivers(Resource):
         if args.get("abbreviation"):
             for racer in structured_info.values():
                 if racer.abbreviation == args.get("abbreviation"):
-                    if args.get("format") == 'xml':
-                        root = ET.Element('racer')
-
-                        drivers_name = ET.SubElement(root, 'name')
-                        drivers_name.text = racer.name
-
-                        drivers_team = ET.SubElement(root, 'team')
-                        drivers_team.text = racer.team
-
-                        drivers_lap_time = ET.SubElement(root, 'lap_time')
-                        drivers_lap_time.text = racer.lap_time_str
-                        return ET.tostring(root).decode()
-                    else:
-                        info_about_racer = {'name': racer.name,
-                                            'team': racer.team,
-                                            'lap_time': racer.lap_time_str}
-                        return jsonify(info_about_racer)
+                    info_about_racer = {racer.name: {'name': racer.name,
+                                                     'team': racer.team,
+                                                     'lap_time': racer.lap_time_str}}
+                    return generate_output_data(info_about_racer, args.get("format"))
         prepared_info_for_report = info_for_output(structured_info, args.get("order"))
-        if args.get("format") == 'xml':
-            return generate_xml_data(prepared_info_for_report)
-        else:
-            return generate_json_data(prepared_info_for_report)
+        return generate_output_data(prepared_info_for_report, args.get("format"))
+
+
+def generate_output_data(info_for_api, format_for_output):
+    if format_for_output == 'xml':
+        return Response(dict2xml(info_for_api, wrap="racers", indent="  "), mimetype='application/xml')
+    else:
+        return jsonify(info_for_api)
 
 
 def info_for_output(structured_info, ordering):
@@ -65,38 +52,14 @@ def info_for_output(structured_info, ordering):
             racer.place = next(racer_number_sequence)
         else:
             racer.place = '-'
-    return prepared_info_for_report
 
-
-def generate_json_data(racers_list):
     info_for_api = [
         {racer.name: {'place': racer.place,
                       'name': racer.name,
                       'team': racer.team,
-                      'lap_time': racer.lap_time_str}} for racer in racers_list
+                      'lap_time': racer.lap_time_str}} for racer in prepared_info_for_report
     ]
-    return jsonify(info_for_api)
-
-
-def generate_xml_data(racers_list):
-    root = ET.Element('racers')
-    for racer in racers_list:
-        driver = ET.Element('racer', attrib={'name': racer.name})
-        root.append(driver)
-
-        drivers_place = ET.SubElement(driver, 'place')
-        drivers_place.text = str(racer.place)
-
-        drivers_name = ET.SubElement(driver, 'name')
-        drivers_name.text = racer.name
-
-        drivers_team = ET.SubElement(driver, 'team')
-        drivers_team.text = racer.team
-
-        drivers_lap_time = ET.SubElement(driver, 'lap_time')
-        drivers_lap_time.text = racer.lap_time_str
-
-    return ET.tostring(root).decode()
+    return info_for_api
 
 
 api.add_resource(Report, '/api/v1/report/')
